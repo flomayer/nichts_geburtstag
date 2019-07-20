5c107ab9109f31943e5e074f372038740f774c06
Florian Mayer <40953443+flomayer@users.noreply.github.com>
2019-07-20 15:09:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nichts
+Eine App für meinen Vater, der sich Nichts zu Weihnachten wünscht.<br>
+Release Build: https://github.com/flomayer/nichts/raw/master/app/release/app-release.apk
---
+++ app/src/main/java/com/flomayer/nichtszuweihnachten/Nichts.java
@@ -0,0 +1,13 @@
+package com.flomayer.nichtszuweihnachten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nichts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nichts);
+    }
+}
